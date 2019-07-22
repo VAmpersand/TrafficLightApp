2042efdf516672a273883f428f3f08411dc05478
Viktor Prikolota <VP_ampersand_P@icloud.com>
2019-07-23 00:28:12 +0300

    Initial Commit

---
+++ TrafficLightApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficLightApp
+//
+//  Created by Viktor on 23/07/2019.
+//  Copyright © 2019 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
